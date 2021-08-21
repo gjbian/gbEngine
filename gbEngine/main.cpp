e29ca690af4193e8beada3b1cf9f8f4906c207e0
gjbian <1041335855@qq.com>
2021-08-21 11:47:08 +0800

    add vector and time_measure

--- gbEngine/main.cpp
+++ gbEngine/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+#include "TVector.h"
+#include "FVector4.h"
+
 int main()
 {
   std::cout << "HelloWorld!!" << std::endl;
+  // TVector<int, 4>::UnitTest();
+  FVector4::UnitTest();
   return 0;
 }
